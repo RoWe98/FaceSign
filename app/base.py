953a1afeb66ca3ff7d5e20423e8e2aa0b8d8867c
RoWe98 <wzx8551517@163.com>
2019-08-08 19:51:56 +0800

    Rewrite the readme file

--- app/base.py
+++ app/base.py
@@ -2,11 +2,12 @@
 
 import cv2
 from util import FaceUtil
-
 class FaceDetect:
 
     @staticmethod
     def get_face():
+
+        face_id_list = []
 
         # get training set file
         cascPath = "haarcascade_frontalface_default.xml"
@@ -40,8 +41,11 @@ class FaceDetect:
                 pixel_1 = int(w/2+x)
                 pixel_2 = int(y-h/5)
                 cv2.rectangle(image, (x, y), (x + w, y + w), (0, 255, 0), 2)
-                cv2.putText(image, "face", (pixel_1,pixel_2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                
+
+                # if len(face_id_list):
+                #     cv2.putText(image, "face:"+face_id_list[0], (pixel_1,pixel_2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                # else:
+                cv2.putText(image, "face", (pixel_1, pixel_2), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 0), 2)
             cv2.imshow("Faces found", cv2.resize(image, (1024, 768)))
 
             if cv2.waitKey(1) == ord('q'):
@@ -52,8 +56,7 @@ class FaceDetect:
                 face_pixel = FaceUtil.get_face_pixel(faces)
                 FaceUtil.screenshot(image,face_pixel)
                 face_id = FaceUtil.lockopen()
+                face_id_list.insert(0,face_id)
 
-                cv2.putText(image, "FaceID"+face_id, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                cv2.imshow("Faces found", cv2.resize(image, (1024, 768)))
 
 
